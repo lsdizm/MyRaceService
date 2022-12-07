46662a8228af53ac7621f820ec1a419111c8f9d4
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-08 08:56:34 +0900

    스크립트 실행 폴더

--- myrace.h
+++ myrace.h
@@ -1,6 +1,6 @@
-git -C MyRaceService pull
-dotnet publish MyRaceService/my.race.api -c Release -o ../publish
-cp MyRaceService/myrace.service /etc/systemd/system/myrace.service
+git pull
+dotnet publish my.race.api -c Release -o ../publish
+cp myrace.service /etc/systemd/system/myrace.service
 systemctl daemon-reload
 systemctl stop myrace.service
 systemctl enable myrace.service
