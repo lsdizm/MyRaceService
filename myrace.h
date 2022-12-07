fc4c0b34ddea78f54ddc309fcd27427229c1c7b2
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-08 08:55:10 +0900

    스크립트조정

--- myrace.h
+++ myrace.h
@@ -1,8 +1,8 @@
-sudo git pull -C MyRaceService
-sudo systemctl stop myrace.service
-sudo dotnet publish MyRaceService/my.race.api -c Release -o ../publish
-sudo cp MyRaceService/myrace.service /etc/systemd/system/myrace.service
-sudo systemctl daemon-reload
-sudo systemctl enable myrace.service
-sudo systemctl start myrace.service
-sudo systemctl status myrace.service 
+git -C MyRaceService pull
+dotnet publish MyRaceService/my.race.api -c Release -o ../publish
+cp MyRaceService/myrace.service /etc/systemd/system/myrace.service
+systemctl daemon-reload
+systemctl stop myrace.service
+systemctl enable myrace.service
+systemctl start myrace.service
+systemctl status myrace.service 
