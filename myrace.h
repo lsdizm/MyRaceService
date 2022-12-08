24ff74dd8bcb4b8821f27c54a487f82fbfefe233
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-09 08:58:55 +0900

    배포스크립트오류수정

--- myrace.h
+++ myrace.h
@@ -6,8 +6,8 @@ systemctl enable myrace.service
 systemctl enable myraceworker.service
 systemctl stop myrace.service
 systemctl stop myraceworker.service
-dotnet publish my.race.api -c Release -o ../publish
-dotnet publish my.race.myraceworker -c Release -o ../publish
+dotnet publish my.race.api -c Release -o publish/
+dotnet publish my.race.worker -c Release -o publish/
 systemctl start myrace.service
 systemctl start myraceworker.service
 systemctl status myrace.service 
