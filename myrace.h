993ca95ebdf2fabfd754965fe7e4b74c49184388
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-14 13:00:51 +0900

    버전테스트

--- myrace.h
+++ myrace.h
@@ -4,10 +4,10 @@ sudo cp /home/opc/MyRaceService/myraceworker.service /etc/systemd/system/myracew
 sudo systemctl daemon-reload
 sudo systemctl enable myrace.service
 sudo systemctl enable myraceworker.service
-sudo systemctl stop myrace.service
-sudo systemctl stop myraceworker.service
 sudo dotnet publish /home/opc/MyRaceService/my.race.api -c Release -o /home/opc/MyRaceService/publish/
 sudo dotnet publish /home/opc/MyRaceService/my.race.worker -c Release -o /home/opc/MyRaceService/publish/
+sudo systemctl restart myraceworker.service
+sudo systemctl restart myrace.service
 sudo systemctl start myrace.service
 sudo systemctl start myraceworker.service
 sudo systemctl status myrace.service 
