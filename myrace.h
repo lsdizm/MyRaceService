1dc11107df0f693454a57257ee79e08e4e069fc1
burn7 <burn7@ezcaretech.com>
2022-12-14 12:44:10 +0900

    sh 절대경로지정

--- myrace.h
+++ myrace.h
@@ -1,13 +1,13 @@
-git pull
-cp myrace.service /etc/systemd/system/myrace.service
-cp myraceworker.service /etc/systemd/system/myraceworker.service
+git -C /home/opc/MyRaceService pull
+cp /home/opc/MyRaceService/myrace.service /etc/systemd/system/myrace.service
+cp /home/opc/MyRaceService/myraceworker.service /etc/systemd/system/myraceworker.service
 systemctl daemon-reload
 systemctl enable myrace.service
 systemctl enable myraceworker.service
 systemctl stop myrace.service
 systemctl stop myraceworker.service
-dotnet publish my.race.api -c Release -o publish/
-dotnet publish my.race.worker -c Release -o publish/
+dotnet publish /home/opc/MyRaceService/my.race.api -c Release -o /home/opc/MyRaceService/publish/
+dotnet publish /home/opc/MyRaceService/my.race.worker -c Release -o /home/opc/MyRaceService/publish/
 systemctl start myrace.service
 systemctl start myraceworker.service
 systemctl status myrace.service 
