0e3f72fc2cac0007a50e0fddaee46f6c2908a3db
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-16 09:12:00 +0900

    add telegram

--- my.race.api/Controllers/SystemController.cs
+++ my.race.api/Controllers/SystemController.cs
@@ -59,4 +59,25 @@ public class SystemController : ControllerBase
     {
        return Ok("2022-12-15-버전체크 05");
     }
+
+     [HttpGet("telegram")]
+    public async Task<IActionResult> SendTelegram([FromQuery]string message)
+    {
+        var token = "5974273292:AAH_dQslxH-pj78N-PffAwkYoVmbnPtj3bM";
+        var chatId = "415767607";
+        var url = string.Format(@"https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}", token, chatId, message);
+        var httpClient = new HttpClient();
+ 
+        try
+        {
+
+            var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return Ok(result);
+        }
+        catch (Exception ex) 
+        { 
+            return BadRequest(ex.ToString());
+        }
+    }
 }
