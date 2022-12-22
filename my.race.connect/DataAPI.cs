207732f8f42a428493078796627e5197c2348d2c
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-22 12:54:52 +0900

    errorMessage

--- my.race.connect/DataAPI.cs
+++ my.race.connect/DataAPI.cs
@@ -68,22 +68,22 @@ namespace my.race.connect
                             var errorMessage = string.Empty;
                             switch (apiResult?.response?.header?.resultCode)
                             {
-                                case "1": errorMessage = "어플리케이션 에러 (1) :"; break;
-                                case "4": errorMessage = "HTTP 에러 (4) :"; break;
-                                case "12": errorMessage = "해당 오픈 API 서비스가 없거나 폐기됨 (12) :"; break;
-                                case "20": errorMessage = "서비스 접근거부 (20) :"; break;
-                                case "22": errorMessage = "서비스 요청제한횟수 초과에러 (22) :"; break;
-                                case "30":errorMessage = "등록되지 않은 서비스키 (30) :"; break;
-                                case "31": errorMessage = "활용기간 만료 (31) :"; break;
-                                case "32": errorMessage = "등록되지 않은 IP (32) :"; break;
-                                case "99": errorMessage = "기타에러 (99) :"; break;
+                                case "1": errorMessage = "APPLICATION ERROR(1) :"; break;
+                                case "4": errorMessage = "HTTP_ERROR(4) :"; break;
+                                case "12": errorMessage = "NO_OPENAPI_SERVICE_ERROR(12) :"; break;
+                                case "20": errorMessage = "SERVICE_ACCESS_DENIED_ERROR(20) :"; break;
+                                case "22": errorMessage = "LIMITED_NUMBER_OF_SERVICE_REQUESTS_EXCEEDS_ERROR(22) :"; break;
+                                case "30":errorMessage = "SERVICE_KEY_IS_NOT_REGISTERED_ERROR(30) :"; break;
+                                case "31": errorMessage = "DEADLINE_HAS_EXPIRED_ERROR(31) :"; break;
+                                case "32": errorMessage = "UNREGISTERED_IP_ERROR(32) :"; break;
+                                case "99": errorMessage = "UNKNOWN_ERROR(99) :"; break;
                                 default:
                                     break;
                             }
 
                             if (!string.IsNullOrEmpty(errorMessage))
                             {
-                                throw new Exception(errorMessage + " [" + url + "] " + apiResult?.response?.header?.resultMsg);
+                                throw new Exception(errorMessage + " [\"" + url + "\"] " + apiResult?.response?.header?.resultMsg);
                             }
 
                             //break;
